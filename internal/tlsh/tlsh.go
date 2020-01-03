57998acd2a182310953b974b24432d8c6a809f39
Artem <aopoltorzhicky@gmail.com>
2020-01-03 21:02:09 +0300

    Remove unused libs

--- internal/tlsh/tlsh.go
+++ internal/tlsh/tlsh.go
@@ -1,14 +1,3 @@
-/* Copyright 2017 Lukas Rist
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
-
 package tlsh
 
 import (
@@ -62,7 +51,7 @@ func (t *Tlsh) String() string {
 	return hex.EncodeToString(t.Binary())
 }
 
-// Parsing the hash of the string type 
+// Parsing the hash of the string type
 func ParseStringToTlsh(hashString string) (*Tlsh, error) {
 	var code [codeSize]byte
 	hashByte, err := hex.DecodeString(hashString)
