0c00086aecac189e2336e487a8d81fc90c67ed61
Artem <aopoltorzhicky@gmail.com>
2020-02-20 15:28:42 +0300

    Fixes

--- cmd/api/handlers/context.go
+++ cmd/api/handlers/context.go
@@ -9,19 +9,20 @@ import (
 
 // Context -
 type Context struct {
-	ES   *elastic.Elastic
-	RPCs map[string]*noderpc.NodeRPC
-	Dir string
-	DB    *gorm.DB
-	OAUTH *oauth.Config
+	ES    *elastic.Elastic
+	RPCs  map[string]*noderpc.NodeRPC
+	Dir   string
+	DB    database.DB
+	OAUTH oauth.Config
 }
 
 // NewContext -
-func NewContext(e *elastic.Elastic, rpcs map[string]*noderpc.NodeRPC, dir string, db *gorm.DB, oauth *oauth.Config) *Context {
+func NewContext(e *elastic.Elastic, rpcs map[string]*noderpc.NodeRPC, dir string, db database.DB, oauth oauth.Config) *Context {
 	return &Context{
-		ES:   e,
-		RPCs: rpcs,
-		Dir:  dir,
+		ES:    e,
+		RPCs:  rpcs,
+		Dir:   dir,
 		DB:    db,
 		OAUTH: oauth,
+	}
 }
