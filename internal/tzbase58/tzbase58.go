def5240e6c880cbf244a46e3429bbab2831f6969
Roman Serikov <roman.serikov@yandex.ru>
2020-03-30 19:18:24 +0300

    Refactor unpack package

---
+++ internal/tzbase58/tzbase58.go
@@ -0,0 +1,33 @@
+package tzbase58
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+
+	"github.com/btcsuite/btcutil/base58"
+)
+
+// EncodeFromHex - encodes hex string to base58 with prefix
+func EncodeFromHex(input string, prefix []byte) (string, error) {
+	bs, err := hex.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+
+	return EncodeFromBytes(bs, prefix), nil
+}
+
+// EncodeFromBytes - encodes bytes slice to base58 with prefix
+func EncodeFromBytes(input, prefix []byte) string {
+	payload := append(prefix, input...)
+	cksum := checksum(payload)
+	payload = append(payload, cksum...)
+
+	return base58.Encode(payload)
+}
+
+func checksum(input []byte) []byte {
+	h := sha256.Sum256(input)
+	h2 := sha256.Sum256(h[:])
+	return h2[:4]
+}
