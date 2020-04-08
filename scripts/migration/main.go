f83a005fbbc9fdbf2a13ef0eb12a27d1f403f45d
Roman <roman.serikov@yandex.ru>
2020-04-08 21:06:54 +0300

    Run migrations from makefile via env variable (#101)
    Co-authored-by: Michael Zaikin <mz@baking-bad.org>

--- scripts/migration/main.go
+++ scripts/migration/main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/baking-bad/bcdhub/internal/contractparser/consts"
 	"github.com/baking-bad/bcdhub/internal/jsonload"
 	"github.com/baking-bad/bcdhub/scripts/migration/migrations"
 )
 
 func main() {
+	migrationMap := map[string]migrations.Migration{
+		"timestamp":       &migrations.SetTimestampMigration{},
+		"language":        &migrations.SetLanguageMigration{},
+		"contract_alias":  &migrations.SetContractAliasMigration{Network: consts.Mainnet},
+		"operation_alias": &migrations.SetOperationAliasMigration{Network: consts.Mainnet},
+	}
+
+	env := os.Getenv("MIGRATION")
+
+	if env == "" {
+		log.Fatal("MIGRATION env variable is not set.")
+	}
+
+	if _, ok := migrationMap[env]; !ok {
+		log.Fatal("Unknown migration key: ", env)
+	}
+
 	var cfg migrations.Config
 	if err := jsonload.StructFromFile("config.json", &cfg); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	cfg.Print()
 
 	ctx, err := migrations.NewContext(cfg)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer ctx.Close()
 
-	migration := migrations.SetBMDTimestamp{}
-	if err := migration.Do(ctx); err != nil {
-		panic(err)
+	if err := migrationMap[env].Do(ctx); err != nil {
+		log.Fatal(err)
 	}
 
 	migration2 := migrations.SetBMDKeyStrings{}
