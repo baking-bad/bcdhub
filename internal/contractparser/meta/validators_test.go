74fbbd8325b9c82e1120470bc949e3c13a102aa8
Artem <aopoltorzhicky@gmail.com>
2020-07-04 23:18:17 +0300

    Base58 validator

--- internal/contractparser/meta/validators_test.go
+++ internal/contractparser/meta/validators_test.go
@@ -68,6 +68,10 @@ func Test_bytesValidator_Validate(t *testing.T) {
 			name:  "Valid",
 			value: "0123456789abcdef",
 			want:  true,
+		}, {
+			name:  "valid long",
+			value: "c51117f21919bda5ce166ddf0903b34b07c1095ff5fba19165196819cbffce13c4340d2fcdda02f2bdd04fe3b6949729a28749d1b979699be484bdead6801a20",
+			want:  true,
 		},
 	}
 	for _, tt := range tests {
@@ -75,6 +79,40 @@ func Test_bytesValidator_Validate(t *testing.T) {
 			v := &bytesValidator{}
 			if got := v.Validate(tt.value); got != tt.want {
 				t.Errorf("bytesValidator.Validate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_base58Validator_Validate(t *testing.T) {
+	tests := []struct {
+		name  string
+		value interface{}
+		want  bool
+	}{
+		{
+			name:  "invalid signature",
+			value: "edsigtxngyy9YJuyR3agLQTLDkj5SR9ciL4nrhAootCoNnTAVzNyiksgxYFyxzaZFooCcBJ1212VD2Pt2JCmi5jGZ5JbCKti9Ag",
+			want:  false,
+		}, {
+			name:  "signature",
+			value: "edsigtxngyy9YJuyRS3gLQTLDkj5SR9ciL4nrhAootCoNnTAVzNyiksgxYFyxzaZFooCcBJ1212VD2Pt2JCmi5jGZ5JbCKti9Ag",
+			want:  true,
+		}, {
+			name:  "signature 2",
+			value: "edsigtYMZAhSbi3V2SdR7ij7v21ctW63Sr9DUfAZB443XS5zZegUNopKxYEkV87YuTsHsCJqTSmzxvcF4JER98Prc7Jmfk9JkdQ",
+			want:  true,
+		}, {
+			name:  "key_hash",
+			value: "tz1Qc94FFQqyjX4VMPTcCzpnmYWNQYWcBoNw",
+			want:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &base58Validator{}
+			if got := v.Validate(tt.value); got != tt.want {
+				t.Errorf("base58Validator.Validate() = %v, want %v", got, tt.want)
 			}
 		})
 	}
