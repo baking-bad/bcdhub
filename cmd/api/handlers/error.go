2818be949ce519fd46682dab94bf8c84f2766835
Roman Serikov <roman.serikov@yandex.ru>
2020-07-20 20:25:54 +0300

    Send url to sentry when capturing err

--- cmd/api/handlers/error.go
+++ cmd/api/handlers/error.go
@@ -5,21 +5,30 @@ import (
 	"net/http"
 
 	"github.com/baking-bad/bcdhub/internal/elastic"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 )
 
 func handleError(c *gin.Context, err error, code int) bool {
-	if err != nil {
-		if code == http.StatusUnauthorized {
-			err = errors.New("Invalid authentication")
-		}
-		if code == 0 {
-			code = getErrorCode(err)
-		}
-		c.AbortWithStatusJSON(code, Error{Message: err.Error()})
-		return true
+	if err == nil {
+		return false
 	}
-	return false
+
+	switch code {
+	case http.StatusUnauthorized:
+		err = errors.New("Invalid authentication")
+	case 0:
+		code = getErrorCode(err)
+
+		if code == http.StatusInternalServerError {
+			if hub := sentrygin.GetHubFromContext(c); hub != nil {
+				hub.CaptureMessage(err.Error())
+			}
+		}
+	}
+
+	c.AbortWithStatusJSON(code, Error{Message: err.Error()})
+	return true
 }
 
 func getErrorCode(err error) int {
