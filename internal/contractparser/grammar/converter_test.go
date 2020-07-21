5c83aca50aace2beefc6c22c5faf8aee09f8a269
Artem <aopoltorzhicky@gmail.com>
2020-07-21 18:50:40 +0300

    Translator: michelson -> micheline

---
+++ internal/contractparser/grammar/converter_test.go
@@ -0,0 +1,54 @@
+package grammar
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/valyala/fastjson"
+)
+
+func TestConverter_FromFile(t *testing.T) {
+	files, err := ioutil.ReadDir("./tests/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c, err := NewConverter(
+		WithDefaultGrammar(),
+	)
+	if err != nil {
+		t.Errorf("Converter.NewConverter() error = %v", err)
+		return
+	}
+	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+
+		t.Run(file.Name(), func(t *testing.T) {
+			resultFilename := fmt.Sprintf("tests/%s/code.json", file.Name())
+			resultBytes, err := ioutil.ReadFile(resultFilename)
+			if err != nil {
+				t.Errorf("ioutil.ReadFile() error = %v", err)
+				return
+			}
+			want, err := fastjson.ParseBytes(resultBytes)
+			if err != nil {
+				t.Errorf("fastjson.ParseBytes() error = %v", err)
+				return
+			}
+
+			filename := fmt.Sprintf("tests/%s/code.tz", file.Name())
+			got, err := c.FromFile(filename)
+			if err != nil {
+				t.Errorf("Converter.FromFile() error = %v", err)
+				return
+			}
+
+			assert.JSONEq(t, got.String(), want.String())
+		})
+	}
+}
