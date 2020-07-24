619108be55a9890d8d4fa44ccf9fbcde911db880
Roman Serikov <roman.serikov@yandex.ru>
2020-07-24 16:40:07 +0300

    fix fa migration

--- scripts/migration/migrations/set_fa.go
+++ scripts/migration/migrations/set_fa.go
@@ -1,15 +1,15 @@
 package migrations
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/baking-bad/bcdhub/internal/config"
+	"github.com/baking-bad/bcdhub/internal/contractparser"
+	"github.com/baking-bad/bcdhub/internal/helpers"
 	"github.com/baking-bad/bcdhub/internal/logger"
 	"github.com/schollz/progressbar/v3"
 
 	"github.com/baking-bad/bcdhub/internal/contractparser/consts"
-	"github.com/baking-bad/bcdhub/internal/contractparser/kinds"
 	"github.com/baking-bad/bcdhub/internal/contractparser/meta"
 	"github.com/baking-bad/bcdhub/internal/elastic"
 )
@@ -35,80 +35,39 @@ func (m *SetFA) Do(ctx *config.Context) error {
 	}
 
 	logger.Info("Found %d contracts", len(contracts))
+	updates := make([]elastic.Model, 0)
 
-	for name, entrypoints := range ctx.Interfaces {
-		logger.Info("Found %s interface", name)
+	bar := progressbar.NewOptions(len(contracts), progressbar.OptionSetPredictTime(false), progressbar.OptionClearOnFinish())
+	for _, c := range contracts {
+		bar.Add(1) //nolint
 
-		updates := make([]elastic.Model, 0)
-		bar := progressbar.NewOptions(len(contracts), progressbar.OptionSetPredictTime(false), progressbar.OptionClearOnFinish())
-		for _, c := range contracts {
-			bar.Add(1) //nolint
-
-			m, err := meta.GetMetadata(ctx.ES, c.Address, consts.PARAMETER, "PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS")
+		m, err := meta.GetMetadata(ctx.ES, c.Address, consts.PARAMETER, "PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS")
+		if err != nil {
+			if !strings.Contains(err.Error(), "Unknown metadata sym link") {
+				return err
+			}
+			m, err = meta.GetMetadata(ctx.ES, c.Address, consts.PARAMETER, "PtYuensgYBb3G3x1hLLbCmcav8ue8Kyd2khADcL5LsT5R1hcXex")
 			if err != nil {
-				if !strings.Contains(err.Error(), "Unknown metadata sym link") {
-					return err
-				}
-				m, err = meta.GetMetadata(ctx.ES, c.Address, consts.PARAMETER, "PtYuensgYBb3G3x1hLLbCmcav8ue8Kyd2khADcL5LsT5R1hcXex")
-				if err != nil {
-					return err
-				}
+				return err
 			}
-			if !findInterface(m, entrypoints) {
-				continue
-			}
-
-			c.Tags = append(c.Tags, name)
-			updates = append(updates, &c)
 		}
 
-		if err := ctx.ES.BulkUpdate(updates); err != nil {
-			logger.Errorf("ctx.ES.BulkUpdate error: %v", err)
+		parameter := new(contractparser.Parameter)
+		parameter.Tags = make(helpers.Set)
+		parameter.Metadata = m
+
+		if err := parameter.FindTags(ctx.Interfaces); err != nil {
 			return err
 		}
+
+		c.Tags = append(c.Tags, parameter.Tags.Values()...)
+		updates = append(updates, &c)
+	}
+
+	if err := ctx.ES.BulkUpdate(updates); err != nil {
+		logger.Errorf("ctx.ES.BulkUpdate error: %v", err)
+		return err
 	}
 
 	return nil
-}
-
-func findInterface(metadata meta.Metadata, i []kinds.Entrypoint) bool {
-	root := metadata["0"]
-
-	for _, ie := range i {
-		found := false
-		for _, e := range root.Args {
-			entrypointMeta := metadata[e]
-			if compareEntrypoints(metadata, ie, *entrypointMeta, e) {
-				found = true
-				break
-			}
-		}
-		if !found {
-			return false
-		}
-	}
-	return true
-}
-
-func compareEntrypoints(metadata meta.Metadata, in kinds.Entrypoint, en meta.NodeMetadata, path string) bool {
-	if in.Name != "" && en.Name != in.Name {
-		return false
-	}
-	// fmt.Printf("[in] %+v\n[en] %+v\n\n", in, en)
-	if in.Prim != en.Prim {
-		return false
-	}
-
-	for i, inArg := range in.Args {
-		enPath := fmt.Sprintf("%s/%d", path, i)
-		enMeta, ok := metadata[enPath]
-		if !ok {
-			return false
-		}
-		if !compareEntrypoints(metadata, inArg, *enMeta, enPath) {
-			return false
-		}
-	}
-
-	return true
 }
