6bf382c5736b4128045e1eafbfe18aa760c3ee68
Roman Serikov <roman.serikov@yandex.ru>
2020-09-09 17:51:07 +0300

    removed unused import

--- internal/metrics/operation.go
+++ internal/metrics/operation.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/baking-bad/bcdhub/internal/compiler/compilation"
-	"github.com/baking-bad/bcdhub/internal/contractparser/consts"
 	"github.com/baking-bad/bcdhub/internal/contractparser/stringer"
 	"github.com/baking-bad/bcdhub/internal/database"
 	"github.com/baking-bad/bcdhub/internal/elastic"
