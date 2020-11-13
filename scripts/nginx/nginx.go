786ad04354eb642a624183ae0cdc5b364a5763d4
m-kus <mz@baking-bad.org>
2020-11-13 18:48:47 +0300

    Show verbose description | Add dapps landing meta

--- scripts/nginx/nginx.go
+++ scripts/nginx/nginx.go
@@ -12,10 +12,14 @@ import (
 	"github.com/baking-bad/bcdhub/internal/models/tzip"
 )
 
-const ogTitle = "Better Call Dev"
-const ogDescription = "Tezos smart contract explorer, developer dashboard, and API provider. Easy to spin up / integrate with your sandbox."
-const ogImage = "/img/logo_og.png"
-const pageTitle = "Better Call Dev — Tezos smart contract explorer by Baking Bad"
+const (
+	ogTitle          = "Better Call Dev"
+	ogDescription    = "Tezos smart contract explorer, developer dashboard, and API provider. Easy to spin up / integrate with your sandbox."
+	ogImage          = "/img/logo_og.png"
+	pageTitle        = "Better Call Dev — Tezos smart contract explorer by Baking Bad"
+	dappsTitle       = "Tezos DApps | Better Call Dev"
+	dappsDescription = "Track the Tezos ecosystem growth: aggregated DApps usage stats, DEX token turnover, affiliated smart contracts, screenshots, social links, and more."
+)
 
 const defaultConfTemplate = `server {
 	listen          80;
@@ -39,18 +43,18 @@ const dappLocationTemplate = `
 	location /dapps/{{.slug}} {
 		rewrite ^ /index.html break;
 		sub_filter '<meta property=og:url content=/' '<meta property=og:url content={{.baseUrl}}/dapps/{{.slug}}';
-		sub_filter '<meta property=og:title content="{{.ogTitle}}"' '<meta property=og:title content="{{.name}} | Tezos DApps"';
-		sub_filter '<meta property=og:description content="{{.ogDescription}}"' '<meta property=og:description content="{{.shortDescription}}"';
+		sub_filter '<meta property=og:title content="{{.ogTitle}}"' '<meta property=og:title content="{{.name}}"';
+		sub_filter '<meta property=og:description content="{{.ogDescription}}"' '<meta property=og:description content="{{.description}}"';
 		sub_filter '<meta property=og:image content={{.ogImage}}' '<meta property=og:image content={{.logoURL}}';
 		sub_filter '<meta property=og:image:secure_url content={{.ogImage}}' '<meta property=og:image:secure_url content={{.logoURL}}';
 		sub_filter '<meta name=twitter:image content={{.ogImage}}' '<meta name=twitter:image content={{.logoURL}}';
 		sub_filter '<meta name=twitter:title content="{{.ogTitle}}"' '<meta name=twitter:title content="{{.name}}"';
-		sub_filter '<meta name=twitter:description content="{{.ogDescription}}"' '<meta name=twitter:description content="{{.shortDescription}}"';
-		sub_filter '<title>{{.pageTitle}}</title>' '<title>{{.name}} | Tezos DApps</title>';
+		sub_filter '<meta name=twitter:description content="{{.ogDescription}}"' '<meta name=twitter:description content="{{.description}}"';
+		sub_filter '<title>{{.pageTitle}}</title>' '<title>{{.title}}</title>';
 		sub_filter_once on;
 	}`
 
-func makeDappLocation(dapp tzip.DApp, baseURL string) string {
+func makeDappLocation(dapp tzip.DApp, baseURL string) (string, error) {
 	logoURL := ""
 	for _, picture := range dapp.Pictures {
 		if picture.Type == "logo" {
@@ -62,21 +66,45 @@ func makeDappLocation(dapp tzip.DApp, baseURL string) string {
 	tmpl := template.Must(template.New("").Parse(dappLocationTemplate))
 
 	err := tmpl.Execute(buf, map[string]interface{}{
-		"slug":             dapp.Slug,
-		"name":             dapp.Name,
-		"shortDescription": dapp.ShortDescription,
-		"ogTitle":          ogTitle,
-		"ogDescription":    ogDescription,
-		"ogImage":          ogImage,
-		"pageTitle":        pageTitle,
-		"baseUrl":          baseURL,
-		"logoURL":          logoURL,
+		"slug":          dapp.Slug,
+		"name":          dapp.Name,
+		"description":   dapp.FullDescription,
+		"ogTitle":       ogTitle,
+		"ogDescription": ogDescription,
+		"ogImage":       ogImage,
+		"pageTitle":     pageTitle,
+		"baseUrl":       baseURL,
+		"logoURL":       logoURL,
+		"title":         fmt.Sprintf("%s | %s", dapp.Name, dappsTitle),
 	})
 	if err != nil {
-		logger.Fatal(err)
+		return "", err
 	}
 
-	return buf.String()
+	return buf.String(), nil
+}
+
+func makeDappRootLocation(path, baseURL string) (string, error) {
+	buf := &bytes.Buffer{}
+	tmpl := template.Must(template.New("").Parse(dappLocationTemplate))
+
+	err := tmpl.Execute(buf, map[string]interface{}{
+		"slug":          path,
+		"name":          dappsTitle,
+		"description":   dappsDescription,
+		"ogTitle":       ogTitle,
+		"ogDescription": ogDescription,
+		"ogImage":       ogImage,
+		"pageTitle":     pageTitle,
+		"baseUrl":       baseURL,
+		"logoURL":       ogImage,
+		"title":         dappsTitle,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
 
 func makeNginxConfig(ctx *config.Context, dapps []tzip.DApp, outputDir string) error {
@@ -89,7 +117,20 @@ func makeNginxConfig(ctx *config.Context, dapps []tzip.DApp, outputDir string) e
 
 	var dappLocations strings.Builder
 	for _, dapp := range dapps {
-		dappLocations.WriteString(makeDappLocation(dapp, ctx.Config.BaseURL))
+		loc, err := makeDappLocation(dapp, ctx.Config.BaseURL)
+		if err != nil {
+			return err
+		}
+		dappLocations.WriteString(loc)
+		dappLocations.WriteString("\n")
+	}
+
+	for _, path := range []string{"", "list"} {
+		loc, err := makeDappRootLocation(path, ctx.Config.BaseURL)
+		if err != nil {
+			return err
+		}
+		dappLocations.WriteString(loc)
 		dappLocations.WriteString("\n")
 	}
 
