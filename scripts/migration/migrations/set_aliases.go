9eb93c97bf75ba2980c2b16a73b0bdf97ae82ea9
Roman Serikov <roman.serikov@yandex.ru>
2021-01-11 18:35:08 +0300

    Speed-up set aliases migration

--- scripts/migration/migrations/set_aliases.go
+++ scripts/migration/migrations/set_aliases.go
@@ -2,6 +2,7 @@ package migrations
 
 import (
 	"github.com/baking-bad/bcdhub/internal/config"
+	"github.com/baking-bad/bcdhub/internal/contractparser/consts"
 	"github.com/baking-bad/bcdhub/internal/logger"
 	"github.com/baking-bad/bcdhub/internal/metrics"
 	"github.com/baking-bad/bcdhub/internal/models"
@@ -25,31 +26,35 @@ func (m *SetAliases) Do(ctx *config.Context) error {
 	h := metrics.New(ctx.Contracts, ctx.BigMapDiffs, ctx.Blocks, ctx.Protocols, ctx.Operations, ctx.Schema, ctx.TokenBalances, ctx.TZIP, ctx.Migrations, ctx.Storage, ctx.Bulk, ctx.DB)
 
 	updatedModels := make([]models.Model, 0)
-	for i := range ctx.Config.Scripts.Networks {
-		logger.Info("Receiving aliases for %s...", ctx.Config.Scripts.Networks[i])
+	logger.Info("Receiving aliases for %s...", consts.Mainnet)
 
-		aliases, err := ctx.TZIP.GetAliasesMap(ctx.Config.Scripts.Networks[i])
+	aliases, err := ctx.TZIP.GetAliasesMap(consts.Mainnet)
+	if err != nil {
+		return err
+	}
+	logger.Info("Received %d aliases", len(aliases))
+
+	if len(aliases) == 0 {
+		return nil
+	}
+
+	addresses := make([]string, 0, len(aliases))
+	for address := range aliases {
+		addresses = append(addresses, address)
+	}
+
+	for _, field := range []string{"source.or", "destination.or", "delegate.or"} {
+		filter := map[string]interface{}{
+			"network": consts.Mainnet,
+			field:     addresses,
+		}
+
+		operations, err := ctx.Operations.Get(filter, 0, false)
 		if err != nil {
-			if ctx.Storage.IsRecordNotFound(err) {
-				continue
-			}
 			return err
 		}
-		logger.Info("Received %d aliases", len(aliases))
 
-		if len(aliases) == 0 {
-			continue
-		}
-
-		networkFilter := map[string]interface{}{
-			"network": ctx.Config.Scripts.Networks[i],
-		}
-
-		operations, err := ctx.Operations.Get(networkFilter, 0, false)
-		if err != nil {
-			return err
-		}
-		logger.Info("Got %d operations", len(operations))
+		logger.Info("Got %d operations for %s", len(operations), field)
 
 		for i := range operations {
 			if flag, err := h.SetOperationAliases(&operations[i]); flag {
@@ -58,13 +63,20 @@ func (m *SetAliases) Do(ctx *config.Context) error {
 				return err
 			}
 		}
+	}
 
-		contracts, err := ctx.Contracts.GetMany(networkFilter)
+	for _, field := range []string{"address.or", "delegate.or"} {
+		filter := map[string]interface{}{
+			"network": consts.Mainnet,
+			field:     addresses,
+		}
+
+		contracts, err := ctx.Contracts.GetMany(filter)
 		if err != nil {
 			return err
 		}
 
-		logger.Info("Got %d contracts", len(contracts))
+		logger.Info("Got %d contracts for %s", len(contracts), field)
 
 		for i := range contracts {
 			if flag, err := h.SetContractAlias(&contracts[i]); flag {
@@ -73,20 +85,20 @@ func (m *SetAliases) Do(ctx *config.Context) error {
 				return err
 			}
 		}
+	}
 
-		transfers, err := ctx.Transfers.GetAll(ctx.Config.Scripts.Networks[i], 0)
-		if err != nil {
+	transfers, err := ctx.Transfers.GetAll(consts.Mainnet, 0)
+	if err != nil {
+		return err
+	}
+
+	logger.Info("Got %d transfers", len(transfers))
+
+	for i := range transfers {
+		if flag, err := h.SetTransferAliases(&transfers[i]); flag {
+			updatedModels = append(updatedModels, &transfers[i])
+		} else if err != nil {
 			return err
-		}
-
-		logger.Info("Got %d transfers", len(transfers))
-
-		for i := range transfers {
-			if flag, err := h.SetTransferAliases(&transfers[i]); flag {
-				updatedModels = append(updatedModels, &transfers[i])
-			} else if err != nil {
-				return err
-			}
 		}
 	}
 
