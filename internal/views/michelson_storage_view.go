170979e8b19e51e068feafe6edb76eded2700a48
Artem <aopoltorzhicky@gmail.com>
2021-02-16 14:31:06 +0300

    Fix: views execution when parameter is empty

--- internal/views/michelson_storage_view.go
+++ internal/views/michelson_storage_view.go
@@ -17,8 +17,12 @@ type MichelsonStorageView struct {
 
 // NewMichelsonStorageView -
 func NewMichelsonStorageView(impl tzip.ViewImplementation, name string) *MichelsonStorageView {
+	var parameter []byte
+	if !impl.MichelsonStorageView.IsParameterEmpty() {
+		parameter = impl.MichelsonStorageView.Parameter
+	}
 	return &MichelsonStorageView{
-		Parameter:  impl.MichelsonStorageView.Parameter,
+		Parameter:  parameter,
 		ReturnType: impl.MichelsonStorageView.ReturnType,
 		Code:       impl.MichelsonStorageView.Code,
 		Name:       name,
@@ -28,15 +32,21 @@ func NewMichelsonStorageView(impl tzip.ViewImplementation, name string) *Michels
 // GetCode -
 func (msv *MichelsonStorageView) GetCode(storageType gjson.Result) (gjson.Result, error) {
 	var script strings.Builder
-	script.WriteString(`[{ "prim": "parameter", "args": [{"prim": "pair", "args": [`)
-	script.Write(msv.Parameter)
-	script.WriteString(", ")
-	script.WriteString(storageType.String())
-	script.WriteString(`]}]}, {"prim": "storage", "args": [{"prim": "option", "args": [`)
+	script.WriteString(`[{"prim":"parameter","args":[`)
+	if msv.Parameter != nil {
+		script.WriteString(`{"prim":"pair","args":[`)
+		script.Write(msv.Parameter)
+		script.WriteString(",")
+		script.WriteString(storageType.String())
+		script.WriteString("]}")
+	} else {
+		script.WriteString(storageType.String())
+	}
+	script.WriteString(`]},{"prim":"storage","args":[{"prim":"option","args":[`)
 	script.Write(msv.ReturnType)
-	script.WriteString(`]}]},  {"prim": "code", "args": [[{"prim": "CAR"}, `)
+	script.WriteString(`]}]},{"prim":"code","args":[[{"prim":"CAR"},`)
 	script.Write(msv.Code)
-	script.WriteString(`, {"prim": "SOME"}, { "prim": "NIL", "args": [{ "prim": "operation" }]}, { "prim": "PAIR" } ]]}]`)
+	script.WriteString(`,{"prim":"SOME"},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PAIR"}]]}]`)
 	return gjson.Parse(script.String()), nil
 }
 
@@ -48,10 +58,14 @@ func (msv *MichelsonStorageView) Parse(response gjson.Result, output interface{}
 // GetParameter -
 func (msv *MichelsonStorageView) GetParameter(parameter string, storageValue gjson.Result) (gjson.Result, error) {
 	var script strings.Builder
-	script.WriteString(`{"prim": "Pair", "args": [`)
-	script.WriteString(parameter)
-	script.WriteString(", ")
-	script.WriteString(storageValue.String())
-	script.WriteString(`]}`)
+	if msv.Parameter != nil {
+		script.WriteString(`{"prim":"Pair","args":[`)
+		script.WriteString(parameter)
+		script.WriteString(",")
+		script.WriteString(storageValue.String())
+		script.WriteString(`]}`)
+	} else {
+		script.WriteString(storageValue.String())
+	}
 	return gjson.Parse(script.String()), nil
 }
