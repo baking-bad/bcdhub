c76d6a445cb02c61ea5e5da4aaa256c0ae3ccfaf
Artem <aopoltorzhicky@gmail.com>
2021-04-25 19:06:00 +0300

    Fix: array JSON schema

--- internal/bcd/ast/jsonschema.go
+++ internal/bcd/ast/jsonschema.go
@@ -133,27 +133,28 @@ func setChildSchema(child Node, required bool, parent *JSONSchema) error {
 		return err
 	}
 
-	if len(s.Properties) == 0 {
+	switch {
+	case parent.Type == JSONSchemaTypeArray:
+		if parent.Items.Properties == nil {
+			parent.Items.Properties = make(map[string]*JSONSchema)
+		}
+		if parent.Items.Required == nil {
+			parent.Items.Required = make([]string, 0)
+		}
+		fields := mergePropertiesMap(s.Properties, parent.Items.Properties, required, false)
+		parent.Items.Required = append(parent.Items.Required, fields.reqs...)
+		if len(parent.Items.Properties) == 0 {
+			parent.Items.Properties[child.GetName()] = s
+		}
+		return nil
+	case len(s.Properties) == 0:
 		parent.Properties[child.GetName()] = s
 		return nil
-	}
-
-	if parent.Items == nil {
+	default:
 		fields := mergePropertiesMap(s.Properties, parent.Properties, required, false)
 		parent.Required = append(parent.Required, fields.reqs...)
 		return nil
 	}
-
-	if parent.Items.Properties == nil {
-		parent.Items.Properties = make(map[string]*JSONSchema)
-	}
-	if parent.Items.Required == nil {
-		parent.Items.Required = make([]string, 0)
-	}
-	fields := mergePropertiesMap(s.Properties, parent.Items.Properties, required, false)
-	parent.Items.Required = append(parent.Items.Required, fields.reqs...)
-
-	return nil
 }
 
 func setChildSchemaForMap(child Node, needXTitle bool, parent *JSONSchema) error {
