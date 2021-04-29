f74db38c5f861cbec954cbfb46fbb29d8bd8bd43
Artem Poltorzhitskiy <aopoltorzhicky@gmail.com>
2021-04-29 13:36:14 +0300

    Fix: int forging (#615)

--- internal/bcd/forge/int_test.go
+++ internal/bcd/forge/int_test.go
@@ -49,6 +49,11 @@ func TestInt_Unforge(t *testing.T) {
 			data: []byte{0xc0, 0xf9, 0xb9, 0xd4, 0xc7, 0x23},
 			want: 6,
 			val:  big.NewInt(-610913435200),
+		}, {
+			name: "int 8b937f02",
+			data: []byte{0x8b, 0x93, 0x7f, 0x02},
+			want: 3,
+			val:  big.NewInt(1041611),
 		},
 	}
 	for _, tt := range tests {
@@ -65,59 +70,6 @@ func TestInt_Unforge(t *testing.T) {
 			}
 			if val.IntValue.Cmp(tt.val) != 0 {
 				t.Errorf("Int.Unforge() parsed value = %v, want %v", val.IntValue.Int64(), tt.val.Int64())
-				return
-			}
-		})
-	}
-}
-
-func TestInt_decode(t *testing.T) {
-	tests := []struct {
-		name    string
-		data    []byte
-		want    *big.Int
-		wantErr bool
-	}{
-		{
-			name: "Small int",
-			data: []byte{0x06},
-			want: big.NewInt(6),
-		},
-		{
-			name: "Negative small int",
-			data: []byte{0x46},
-			want: big.NewInt(-6),
-		},
-		{
-			name: "Medium int",
-			data: []byte{0x84, 0x0e},
-			want: big.NewInt(900),
-		},
-		{
-			name: "Negative medium int",
-			data: []byte{0xc4, 0x0e},
-			want: big.NewInt(-900),
-		},
-		{
-			name: "Large int",
-			data: []byte{0xba, 0x9a, 0xf7, 0xea, 0x06},
-			want: big.NewInt(917431994),
-		},
-		{
-			name: "Negative large int",
-			data: []byte{0xc0, 0xf9, 0xb9, 0xd4, 0xc7, 0x23},
-			want: big.NewInt(-610913435200),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			val := NewInt()
-			if err := val.decode(tt.data); (err != nil) != tt.wantErr {
-				t.Errorf("Int.decode() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if tt.want.Cmp(val.IntValue.Int) != 0 {
-				t.Errorf("Int.decode() want = %v, got %v", tt.want, val.IntValue)
 				return
 			}
 		})
