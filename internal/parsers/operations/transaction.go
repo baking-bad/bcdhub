d0e7924021cc36be36569e6b04056b3db4636073
Artem <aopoltorzhicky@gmail.com>
2022-02-13 04:17:27 +0300

    Golden temporary fix

--- internal/parsers/operations/transaction.go
+++ internal/parsers/operations/transaction.go
@@ -154,8 +154,8 @@ func (p Transaction) appliedHandler(item noderpc.Operation, tx *operation.Operat
 			return err
 		}
 		if len(storageStrings) > 0 {
-			if len(storageStrings) > 1000 { // golden fix for postgres limit
-				storageStrings = storageStrings[:1000]
+			if len(storageStrings) > 100 { // golden fix for postgres limit
+				storageStrings = storageStrings[:100]
 			}
 			tx.StorageStrings = storageStrings
 		}
@@ -194,8 +194,8 @@ func (p Transaction) getEntrypoint(tx *operation.Operation) error {
 		return err
 	}
 	if len(parameterStrings) > 0 {
-		if len(parameterStrings) > 1000 { // golden fix for postgres limit
-			parameterStrings = parameterStrings[:1000]
+		if len(parameterStrings) > 100 { // golden fix for postgres limit
+			parameterStrings = parameterStrings[:100]
 		}
 		tx.ParameterStrings = parameterStrings
 	}
