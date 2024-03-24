f12d8564fa6a16b405cec454c14a63a6d694afdc
Artem <aopoltorzhicky@gmail.com>
2024-03-24 13:20:09 +0100

    Remove dailynet

--- build/indexer/entrypoint.sh
+++ build/indexer/entrypoint.sh
@@ -2,6 +2,5 @@
 
 curl https://teztnets.xyz/teztnets.json > /teztnets.json
 mondaynet_rpc=$(cat /teztnets.json | jq '. | to_entries | map(select(.key | startswith("monday"))) | map(.value.rpc_url)[0]')
-dailynet_rpc=$(cat /teztnets.json | jq '. | to_entries | map(select(.key | startswith("daily"))) | map(.value.rpc_url)[0]')
 
-DAILYNET_RPC=$dailynet_rpc MONDAYNET_RPC=$mondaynet_rpc /go/bin/indexer
+MONDAYNET_RPC=$mondaynet_rpc /go/bin/indexer
