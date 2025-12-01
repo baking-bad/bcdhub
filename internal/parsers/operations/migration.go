d0d59cb1d689b8c1a5b11339687675349d8111e1
Konst <johnysalvesen@gmail.com>
2025-12-01 15:56:25 +0300

    Added tallinnnet

--- internal/parsers/operations/migration.go
+++ internal/parsers/operations/migration.go
@@ -70,7 +70,8 @@ func (m Migration) Parse(ctx context.Context, data noderpc.Operation, operation 
 		"PsParisCZo7KAh1Z1smVd9ZMZ1HHn5gkzbM94V3PLCpknFWhUAi",
 		"PsQuebecnLByd3JwTiGadoG4nGWi3HYiLXUjkibeFV8dCFeVMUg",
 		"PsRiotumaAMotcRoDWW1bysEhQy2n1M5fy8JgRp8jjRfHGmfeA7",
-		"PtSeouLouXkxhg39oWzjxDWaCydNfR3RxCUrNe4Q9Ro8BTehcbh":
+		"PtSeouLouXkxhg39oWzjxDWaCydNfR3RxCUrNe4Q9Ro8BTehcbh",
+		"PtTALLiNtPec7mE7yY4m3k26J8Qukef3E3ehzhfXgFZKGtDdAXu":
 		return m.fromLazyStorageDiff(ctx, data, operation, store)
 	default:
 		return errors.Errorf("unknown protocol for migration parser: %s", protocol)
