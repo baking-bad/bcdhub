2ef6d0828c588cf58bbc38ee2ac651306ed206e9
Konst <johnysalvesen@gmail.com>
2026-01-19 16:36:03 +0300

    Added tezlink network

--- internal/models/types/network.go
+++ internal/models/types/network.go
@@ -39,6 +39,7 @@ const (
 	Seoulnet
 	Shadownet
 	Tallinnnet
+	Tezlink
 )
 
 var networkNames = map[Network]string{
@@ -69,6 +70,7 @@ var networkNames = map[Network]string{
 	Seoulnet:     "seoulnet",
 	Shadownet:    "shadownet",
 	Tallinnnet:   "tallinnnet",
+	Tezlink:      "tezlink",
 }
 
 var namesToNetwork = map[string]Network{
@@ -99,6 +101,7 @@ var namesToNetwork = map[string]Network{
 	"seoulnet":     Seoulnet,
 	"shadownet":    Shadownet,
 	"tallinnnet":   Tallinnnet,
+	"tezlink":      Tezlink,
 }
 
 // String - convert enum to string for printing
